2cd2f0611f604f036f53c2cfac23ae9823a05bfe
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-22 20:37:52 +0530

    Create ComputerEm.jsx

---
+++ front-vit/src/Pages/Component/ComputerEm.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Component/Foooter.jsx";
+import Navbar from "./Component/Navbar.jsx";
+
+function ComputerEm() {
+  return (
+    <div>
+      <Navbar />
+      <Footer/>
+    </div>
+  )
+}
+
+export default ComputerEm
